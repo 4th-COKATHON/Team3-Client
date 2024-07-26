fd8884b32350d137a0a430888221405ff8215839
letthem <suyeon3662@naver.com>
2024-07-27 05:38:10 +0900

    Feat: 취소 nav 연결

--- src/components/New/Confirm.js
+++ src/components/New/Confirm.js
@@ -1,13 +1,12 @@
-import styled from 'styled-components';
+import { useNavigate } from "react-router";
+import styled from "styled-components";
 
-
-
-export const ConfirmWrapper=styled.div`
-display:flex;
-align-items: center;
-margin-top:78px;
-gap: 21px;`
-
+export const ConfirmWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 78px;
+  gap: 21px;
+`;
 
 export const ConfirmButton = styled.button`
   width: 360px;
@@ -15,8 +14,8 @@ export const ConfirmButton = styled.button`
   border-radius: 100px;
   border: 0px;
   text-align: center;
-  background-color: #9D968C;
-  color: #F5F5F5;
+  background-color: #9d968c;
+  color: #f5f5f5;
   font-family: "PT-Bold";
   font-size: 30px;
   font-style: normal;
@@ -25,20 +24,20 @@ export const ConfirmButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: #FFC803;
+    background-color: #ffc803;
     color: #242321;
   }
 `;
 
-
-
 const Confirm = () => {
-    return (
-        <ConfirmWrapper>
-            <ConfirmButton>취소</ConfirmButton>  
-            <ConfirmButton>완료하기</ConfirmButton>
-        </ConfirmWrapper> 
-    );
+  const nav = useNavigate();
+
+  return (
+    <ConfirmWrapper>
+      <ConfirmButton onClick={() => nav(-1)}>취소</ConfirmButton>
+      <ConfirmButton>완료하기</ConfirmButton>
+    </ConfirmWrapper>
+  );
 };
 
 export default Confirm;
